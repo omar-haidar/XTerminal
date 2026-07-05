aa974c433a2744c33ac4d465e52f5aa2adf43a68
omar-haidar <rovox2000@gmail.com>
2026-07-05 10:47:18 +0300

    init

--- app/src/main/java/dev/omar/xterminal/app/crash/CrashReportActivity.java
+++ app/src/main/java/dev/omar/xterminal/app/crash/CrashReportActivity.java
@@ -11,27 +11,29 @@ import dev.omar.xterminal.databinding.ActivityCrashReportBinding;
 
 public class CrashReportActivity extends AppCompatActivity {
     private static final String EXTRA_CRASH_MESSAGE = "EXTRA_CRASH_MESSAGE";
-    private ActivityCrashReportBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityCrashReportBinding.inflate(getLayoutInflater());
+        ActivityCrashReportBinding binding = ActivityCrashReportBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         String message = "No error message!";
 
-        if (getIntent().hasExtra(EXTRA_CRASH_MESSAGE)){
+        if (getIntent().hasExtra(EXTRA_CRASH_MESSAGE)) {
             message = getIntent().getStringExtra(EXTRA_CRASH_MESSAGE);
         }
         binding.logText.setText(message);
         binding.closeButton.setOnClickListener(v -> {
-            finishAffinity();
+            finish();
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(0);
         });
 
     }
 
     public static void initCrashHandler(Application app) {
+        if (app == null) return;
         Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
             String message = Log.getStackTraceString(throwable);
             Intent intent = new Intent(app.getApplicationContext(), CrashReportActivity.class);
